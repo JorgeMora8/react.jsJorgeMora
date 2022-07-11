5888ebba11b1f647dc48e4020c04292429b856cb
Jorge Mora <jorgeandresmm2002@gmail.com>
2022-07-11 16:10:28 -0400

    Modificacion del desafio. React.Js

--- proyectoecommerce/src/App.jsx
+++ proyectoecommerce/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import NavBar from './components/navBar/navbar'
+import NavBar from './components/navBar/navbar'; 
+<script src="https://kit.fontawesome.com/4834373349.js" crossorigin="anonymous"></script>
 
 
 function App() {
--- proyectoecommerce/src/components/navBar/navbar.jsx
+++ proyectoecommerce/src/components/navBar/navbar.jsx
@@ -4,30 +4,19 @@ import React from 'react';
 
 const NavBar = () => {
 
-    const navBarGeneral={backgroundColor:"#352b20", display:"flex", justifyContent: "space-around", alignItems:"center", flexWrap:"wrap"}
-    const eCommerceNombre={fontSize:"1.6rem", color:"#ffffff", fontFamily:"poppins", fontWeight:"600"}; 
-    const navBar ={display:"flex", justifyContent:"center", alignItems:"center", flexWrap:"wrap", listStyleType:"none"}; 
-    const navBarItem={color:"#ffffff", margin:"10px", cursor:"pointer"}; 
-    const btnEstilo ={backgroundColor:"#ffffff", color:"#ff624d", border:"none", fontSize:"20px", padding:"5px 10px", borderRadius:"1rem", cursor:"pointer"}
-
   return (
    
-    <div style={navBarGeneral}>
-
+    <div className='nav'>
         <div>
-           <p style={eCommerceNombre}>Alea</p> 
-        </div>
-        
-        <ul style={navBar}>
-            <li style={navBarItem}>Home</li>
-            <li style={navBarItem}>Informacion</li>
-            <li style={navBarItem}>Opciones</li>
-            <li style={navBarItem}>Contacto</li>
+           <p className='navLogo'>Alea</p> 
+        </div>      
+        <ul className='navBar'>
+            <li className='navBarItem'>Home</li>
+            <li className='navBarItem'>Informacion</li>
+            <li className='navBarItem'>Opciones</li>
+            <li className='navBarItem'>Contacto</li>
         </ul>
-
-        <button style={btnEstilo}>Ingresar</button>
-    
-
+        <button className='btnIngresar'>Ingresar</button>
     </div>
   )
 }
